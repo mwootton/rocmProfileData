3373937eda1ec709ad7e5e325c60a7f5b1a2f908
mwootton <michael.wootton@amd.com>
2026-05-20 19:31:41 -0500

    Remove random unused key (#112)

--- rocmProfileData/settings.py
+++ rocmProfileData/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # Recreate:
 # from django.core.management.utils import get_random_secret_key; get_random_secret_key()
-SECRET_KEY = '@o&a#1a$atwb^!f#h%^)83(_naac0$(g$j2!@os74tq@@mvmsl'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
